e5a521973de110fcb71ba248e587e778e1d03521
Abyss <xatkx1@gmail.com>
2022-08-21 02:22:31 -0400

    video 12

--- script2.js
+++ script2.js
@@ -1,15 +1,23 @@
-let btn = document.querySelector('input');
-let allP = document.querySelectorAll('p');
 let h1 = document.querySelector('h1');
+let textbox1 = document.querySelector('#box1');
+let textbox2 = document.querySelector('#box2');
+let btn = document.getElementById('btn');
+let rs = document.querySelector('.respuesta');
 
-h1.innerText = ''
-h1.style.background = 'red'
-h1.style.textAlign = 'center'
-console.log({btn,allP,h1})
+function calculo(event) {
+    let respuesta = parseInt(textbox1.value) + parseInt(textbox2.value)
+    let span = document.createElement('span');
+    span.innerText = `tu respuesta es: ${respuesta}`
+    span.style.fontSize = '50px'
+    span.style.padding = '50px'
+    
+    span.style.fontWeight = 'bold'
+    span.style.color = "white"
+    span.style.background = 'red'
 
-let img = document.createElement('img');
-img.setAttribute('src','https://indiehoy.com/wp-content/uploads/2022/05/naruto.jpg')
-img.width = 100;
-img.height = 100;
+    rs.style.margin = '50px'
+    rs.appendChild(span)
 
-h1.append(img)
+}
+
+btn.addEventListener('click',calculo)
